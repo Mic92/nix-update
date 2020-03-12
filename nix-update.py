5dd5c19ba48fd7baea30cb5d2715be957346ccf5
Jörg Thalheim <joerg@thalheim.io>
2020-03-12 15:01:56 +0000

    add build/nix-shell/nix run support

--- nix-update.py
+++ nix-update.py
@@ -7,12 +7,14 @@ import re
 import subprocess
 import urllib.request
 import xml.etree.ElementTree as ET
+import os
+import tempfile
 import sys
 from typing import Optional
 from urllib.parse import urlparse
 
 
-def die(msg):
+def die(msg: str) -> None:
     print(msg, file=sys.stderr)
     sys.exit(1)
 
@@ -109,14 +111,33 @@ def parse_args():
     parser = argparse.ArgumentParser()
     help = "File to import rather than default.nix. Examples, ./release.nix"
     parser.add_argument("-f", "--file", default="./.", help=help)
+    parser.add_argument("--build", action="store_true", help="build the package")
+    parser.add_argument("--run", action="store_true", help="provide a shell based on `nix run` with the package in $PATH")
+    parser.add_argument("--shell", action="store_true", help="provide a shell with the package")
     parser.add_argument("attribute", help="Attribute name within the file evaluated")
     parser.add_argument("version", nargs="?", help="Version to update to")
     return parser.parse_args()
 
 
+def nix_shell(filename: str, attribute: str) -> None:
+    with tempfile.NamedTemporaryFile(mode="w") as f:
+        f.write(f"""
+        with import {filename}; mkShell {{ buildInputs = [ {attribute} ]; }}
+        """)
+        f.flush()
+        subprocess.run(["nix-shell", f.name])
+
+
 def main() -> None:
     args = parse_args()
     update(args.file, args.attribute, args.version)
+    if args.build:
+        subprocess.run(["nix", "build", "-f", args.file, args.attribute])
+    if args.run:
+        subprocess.run(["nix", "run", "-f", args.file, args.attribute, "-c", "zsh"])
+
+    if args.shell:
+        nix_shell(args.file, args.attribute)
 
 
 if __name__ == "__main__":
