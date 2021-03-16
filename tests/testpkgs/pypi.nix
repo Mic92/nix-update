2a90f40b9a6701bacfa33a3de58b7b3db1859d9d
Sandro Jäckel <sandro.jaeckel@gmail.com>
2021-03-16 13:13:08 +0100

    tests: switch python-mpd2 to a version that has a .tar.gz download

--- tests/testpkgs/pypi.nix
+++ tests/testpkgs/pypi.nix
@@ -2,15 +2,18 @@
 
 buildPythonPackage rec {
   pname = "python-mpd2";
-  version = "1.0.0";
+  version = "2.0.0";
+
   src = fetchPypi {
     inherit pname version;
     sha256 = "0000000000000000000000000000000000000000000000000000";
   };
+
   checkInputs = [
     pytestCheckHook
     twisted
     mock
   ];
+
   pytestFlagsArray = [ "mpd/tests.py" ];
 }
