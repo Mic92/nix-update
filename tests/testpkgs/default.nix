8eed06941ba9cebe0aaeedd5e3d73bf8f3a3a266
figsoda <figsoda@pm.me>
2022-11-24 13:05:17 -0500

    add integration tests for the diff feature

--- tests/testpkgs/default.nix
+++ tests/testpkgs/default.nix
@@ -1,6 +1,8 @@
 { pkgs ? import <nixpkgs> {} }:
 {
   crate = pkgs.callPackage ./crate.nix {};
+  github = pkgs.callPackage ./github.nix {};
+  gitlab = pkgs.callPackage ./gitlab.nix {};
   pypi = pkgs.python3.pkgs.callPackage ./pypi.nix {};
   sourcehut = pkgs.python3.pkgs.callPackage ./sourcehut.nix {};
   savanna = pkgs.python3.pkgs.callPackage ./savanna.nix {};
