609d012babc72c552883f967fabe1cee15e05801
figsoda <figsoda@pm.me>
2022-12-08 12:36:42 -0500

    add test for updating flake outputs

---
+++ tests/testpkgs/flake.nix
@@ -0,0 +1,7 @@
+{
+  outputs = { self, nixpkgs }: {
+    packages = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system: {
+      crate = nixpkgs.legacyPackages.${system}.callPackage (self + "/crate.nix") { };
+    });
+  };
+}
