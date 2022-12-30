13a62cff6390f537ac4269b243bdfd65cb687d0f
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-12-30 19:49:43 +0100

    flake: switch from `nixos-unstable` to `nixpkgs-unstable`

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Swiss-knife for updating nix packages.";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
