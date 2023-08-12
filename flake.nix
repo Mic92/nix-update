1802595c740163e2af60408bdc9af8cbb70ef71f
Jörg Thalheim <joerg@thalheim.io>
2023-08-12 13:59:52 +0200

    treefmt: use default branch again

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
 
-    treefmt-nix.url = "github:numtide/treefmt-nix/mypy";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
