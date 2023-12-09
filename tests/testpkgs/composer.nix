0a30179f00f5a1a6c8422281b9580138b6f83074
Gaël Reyrol <me@gaelreyrol.dev>
2023-12-09 15:07:39 +0100

    Add support for updating buildComposerProject's vendorHash
    Update tests/testpkgs/default.nix

---
+++ tests/testpkgs/composer.nix
@@ -0,0 +1,17 @@
+{ fetchFromGitHub
+, php
+}:
+
+php.buildComposerProject (finalAttrs: {
+  pname = "castor";
+  version = "0.10.0";
+
+  src = fetchFromGitHub {
+    owner = "jolicode";
+    repo = "castor";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+  };
+
+  vendorHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+})
