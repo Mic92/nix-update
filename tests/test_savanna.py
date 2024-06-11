adf2389338da6a1cb2543b8ab138d82251faa27b
Jörg Thalheim <joerg@thalheim.io>
2024-06-11 07:45:06 +0200

    work around broken savannah

--- tests/test_savanna.py
+++ tests/test_savanna.py
@@ -1,12 +1,25 @@
 import subprocess
+import urllib.error
+import urllib.request
 
 import conftest
+import pytest
 
 from nix_update.options import Options
 from nix_update.update import update
 
 
 def test_update(helpers: conftest.Helpers) -> None:
+    try:
+        response = urllib.request.urlopen(
+            "https://download.savannah.nongnu.org/releases/fileschanged/?C=M&O=D",
+            timeout=5,
+        )
+        response.read()
+    except Exception:
+        # savannah's api seems to have issues lately
+        pytest.xfail("Savana is taking too long to respond")
+
     with helpers.testpkgs() as path:
         opts = Options(attribute="savanna", import_path=str(path))
         update(opts)
