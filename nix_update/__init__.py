5bd27038ff3827113b5c9659ac39d978cfe51428
matt1432 <matt@nelim.org>
2025-03-15 12:37:44 -0400

    feat: add print-commit-message flag

--- nix_update/__init__.py
+++ nix_update/__init__.py
@@ -54,6 +54,11 @@ def parse_args(args: list[str]) -> Options:
         help="URL to the repository to check for a release instead of using the URL in the src attribute of the package",
     )
     parser.add_argument(
+        "--print-commit-message",
+        action="store_true",
+        help="Print commit message to stdout",
+    )
+    parser.add_argument(
         "--write-commit-message",
         metavar="FILE",
         help="Write commit message to FILE",
@@ -145,6 +150,7 @@ def parse_args(args: list[str]) -> Options:
         update_script_args=a.update_script_args,
         subpackages=a.subpackage,
         url=a.url,
+        print_commit_message=a.print_commit_message,
         write_commit_message=a.write_commit_message,
         run=a.run,
         shell=a.shell,
@@ -247,6 +253,11 @@ def git_commit(git_dir: str, package: Package) -> None:
                 ],
                 stdout=None,
             )
+
+
+def print_commit_message(package: Package) -> None:
+    print(format_commit_message(package))
+    print("\n")
 
 
 def write_commit_message(path: str, package: Package) -> None:
@@ -390,6 +401,9 @@ def main(args: list[str] = sys.argv[1:]) -> None:
             # If we have a changelog we will re-eval the package in case it has changed
             package.changelog = eval_attr(options).changelog
         git_commit(git_dir, package)
+
+    if options.print_commit_message:
+        print_commit_message(package)
 
     if options.write_commit_message is not None:
         write_commit_message(options.write_commit_message, package)
