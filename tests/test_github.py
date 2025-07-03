0f0858257f41c1142e5da371a573182cfce36e40
Marcin Serwin <marcin@serwin.dev>
2025-07-03 23:10:27 +0200

    test: add tests for GitHub releases api
    Signed-off-by: Marcin Serwin <marcin@serwin.dev>

--- tests/test_github.py
+++ tests/test_github.py
@@ -7,13 +7,44 @@ from nix_update import main
 from tests import conftest
 
 
+def test_github_feed(helpers: conftest.Helpers) -> None:
+    with helpers.testpkgs(init_git=True) as path:
+        main(["--file", str(path), "--commit", "github"])
+        version = subprocess.run(
+            [
+                "nix",
+                "eval",
+                "--raw",
+                "--extra-experimental-features",
+                "nix-command",
+                "-f",
+                path,
+                "github.version",
+            ],
+            check=True,
+            text=True,
+            stdout=subprocess.PIPE,
+        ).stdout.strip()
+        assert tuple(map(int, version.split("."))) >= (8, 5, 2)
+        commit = subprocess.run(
+            ["git", "-C", path, "log", "-1"],
+            text=True,
+            stdout=subprocess.PIPE,
+            check=True,
+        ).stdout.strip()
+        print(commit)
+        assert version in commit
+        assert "github" in commit
+        assert "https://github.com/sharkdp/fd/compare/v8.0.0...v" in commit
+
+
 @pytest.mark.skipif(
     "GITHUB_TOKEN" not in os.environ,
     reason="No GITHUB_TOKEN environment variable set",
 )
-def test_github_api(helpers: conftest.Helpers) -> None:
+def test_github_releases(helpers: conftest.Helpers) -> None:
     with helpers.testpkgs(init_git=True) as path:
-        main(["--file", str(path), "--commit", "github"])
+        main(["--file", str(path), "--commit", "github", "--use-github-releases"])
         version = subprocess.run(
             [
                 "nix",
