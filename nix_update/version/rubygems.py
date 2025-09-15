2085763cf825c2b29ef36c8ee93e34d95353a0bc
Jörg Thalheim <joerg@thalheim.io>
2025-09-15 12:49:36 +0200

    rubygems: only split once

--- nix_update/version/rubygems.py
+++ nix_update/version/rubygems.py
@@ -19,7 +19,7 @@ def fetch_rubygem_versions(url: ParseResult) -> list[Version]:
         return []
     parts = url.path.split("/")
     gem = parts[-1]
-    gem_name, _ = gem.rsplit("-")
+    gem_name, _ = gem.rsplit("-", 1)
     versions_url = f"https://rubygems.org/api/v1/versions/{gem_name}.json"
     info(f"fetch {versions_url}")
     with urllib.request.urlopen(versions_url, timeout=DEFAULT_TIMEOUT) as resp:
