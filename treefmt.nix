7d0c6bc1e735ac4dc3abf05b6d1e866a8e8fcd42
Jörg Thalheim <joerg@thalheim.io>
2025-12-11 21:53:47 +0100

    hostPlatform -> stdenv.hostPlatform

--- treefmt.nix
+++ treefmt.nix
@@ -6,12 +6,12 @@
     { pkgs, ... }:
     {
       treefmt = {
-        flakeCheck = pkgs.hostPlatform.system != "riscv64-linux";
+        flakeCheck = pkgs.stdenv.hostPlatform.system != "riscv64-linux";
 
         # Used to find the project root
         projectRootFile = "flake.lock";
 
-        programs.deno.enable = pkgs.hostPlatform.system != "x86_64-darwin";
+        programs.deno.enable = pkgs.stdenv.hostPlatform.system != "x86_64-darwin";
         programs.mypy.enable = true;
 
         programs.yamlfmt.enable = true;
