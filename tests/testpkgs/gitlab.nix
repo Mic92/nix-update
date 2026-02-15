39e5cd5899e625f04d8e73b957ff403aecd04499
Jörg Thalheim <joerg@thalheim.io>
2026-02-15 17:48:18 +0100

    tests: switch gitlab test from phosh to caps2esc
    gitlab.gnome.org returns HTTP 501 for archive downloads, causing
    persistent CI failures. Switch to caps2esc on gitlab.com which still
    exercises the group parameter in fetchFromGitLab and diff URL
    generation.

--- tests/testpkgs/gitlab.nix
+++ tests/testpkgs/gitlab.nix
@@ -1,13 +1,12 @@
 { stdenv, fetchFromGitLab }:
 
 stdenv.mkDerivation rec {
-  pname = "phosh";
-  version = "0.20.0";
+  pname = "caps2esc";
+  version = "0.1.3";
 
   src = fetchFromGitLab {
-    domain = "gitlab.gnome.org";
-    group = "world";
-    owner = "phosh";
+    group = "interception";
+    owner = "linux/plugins";
     repo = pname;
     rev = "v${version}";
     sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
