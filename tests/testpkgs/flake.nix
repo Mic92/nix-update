ad8ffdfb02185a7bf97044db5d1ff4fabf107d14
Jörg Thalheim <joerg@thalheim.io>
2026-06-11 08:29:34 +0200

    tests: introduce pkgs alias in testpkgs flake
    Shorten repeated nixpkgs.legacyPackages.${system} references before
    adding more packages that need overrides.

--- tests/testpkgs/flake.nix
+++ tests/testpkgs/flake.nix
@@ -10,14 +10,16 @@
       nixpkgs,
     }:
     {
-      packages = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system: {
-        crate = nixpkgs.legacyPackages.${system}.callPackage (self + "/crate.nix") { };
-        flake-use-update-script = nixpkgs.legacyPackages.${system}.callPackage (
-          self + "/flake-use-update-script.nix"
-        ) { };
-        nuget-deps-generate = nixpkgs.legacyPackages.${system}.callPackage (
-          self + "/nuget-deps-generate"
-        ) { };
-      });
+      packages = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          crate = pkgs.callPackage (self + "/crate.nix") { };
+          flake-use-update-script = pkgs.callPackage (self + "/flake-use-update-script.nix") { };
+          nuget-deps-generate = pkgs.callPackage (self + "/nuget-deps-generate") { };
+        }
+      );
     };
 }
